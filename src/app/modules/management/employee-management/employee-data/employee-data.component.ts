531725effef09bbe570971b35f27ea2339151c72
Rifqi-dev <firlianrifqi22@gmail.com>
2023-10-30 23:16:45 +0700

    add(action):delete action on employee

--- src/app/modules/management/employee-management/employee-data/employee-data.component.ts
+++ src/app/modules/management/employee-management/employee-data/employee-data.component.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
+import { Observable, Observer } from 'rxjs';
 import { NewEmployee, employeeList } from 'src/app/models/employee.model';
 import { RoleGuardService } from 'src/app/shared/service/auth/role-guard.service';
 import { EmployeeService } from 'src/app/shared/service/employee/employee.service';
+import { NotificationService } from 'src/app/shared/service/notification/notification.service';
 import { OptionService } from 'src/app/shared/service/option/option.service';
 
 type response = {
@@ -14,6 +19,21 @@ type companyOption = {
   id: number;
   companyName: string;
 };
+
+type addEmployeeData = {
+  id: string
+  name: string
+  email: string
+  phoneNumber: string
+  gender: string
+  position: string
+  address: string
+  companyId: string
+  employeeId: string
+  photoPath: string
+  updatedAt: string
+  createdAt: string
+}
 
 @Component({
   selector: 'app-employee-data',
@@ -49,12 +69,17 @@ export class EmployeeDataComponent implements OnInit {
     },
   ];
 
+  isAddLoading!:boolean
   modalVisible!: boolean;
   employeeForm: FormGroup;
+   pictureList: NzUploadFile[] = [];
+   confirmModal?: NzModalRef;
   constructor(
     private employeeService: EmployeeService,
     private roleGuardService: RoleGuardService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private modal: NzModalService,
+     private notification: NotificationService
   ) {
     this.employeeForm = this.fb.group({
       name: ['', Validators.compose([Validators.required])],
@@ -86,8 +111,29 @@ export class EmployeeDataComponent implements OnInit {
       );
   }
 
+  handleDelete(employee:employeeList):void{
+     this.confirmModal = this.modal.confirm({
+      nzTitle: 'Do you Want to delete these items?',
+      nzContent:
+        'When clicked the OK button, these item will removed and you never see it again!',
+      nzOnOk: () => {
+    this.employeeService.deleteEmployee(employee.id).subscribe(
+      (res:{message:string,status:string})=>{
+        this.notification.showNotification('check', '#52c41a', res.message);
+        window.location.reload()
+      },
+      error =>{
+  this.notification.showNotification('warning',
+          '#eb2f96', error.error.message);
+}
+    )
+  }
+})}
+  
+
   confirmAddEmployee(): void {
     if (!this.employeeForm.valid) return;
+    this.isAddLoading = true
     const payload: NewEmployee = {
       name: this.employeeForm.value.name,
       email: this.employeeForm.value.email,
@@ -99,6 +145,74 @@ export class EmployeeDataComponent implements OnInit {
       companyId: this.roleGuardService.getUserInfo().id,
     };
 
-    console.log(payload);
+    this.employeeService.createEmployee(payload).subscribe(
+      (res:{
+        message:string,
+        status:string,
+        data:addEmployeeData
+      })=>{
+        this.isAddLoading = false
+        this.notification.showNotification('check', '#52c41a', res.message);
+        if(res.data.id){
+          this.uploadImage(res.data.id)
+        }
+      // window.location.reload()
+      },
+      error=>{
+        this.isAddLoading = false
+        console.log(error)
+        this.notification.showNotification(
+          'warning',
+          '#eb2f96',
+          error.error.error.error
+        );
+      }
+    )
   }
+
+  uploadImage(employeeId:string):void {
+    if(!employeeId)return
+     const formData = new FormData();
+    formData.append('image', this.pictureList[0] as any);
+    this.employeeService.uploadImage(employeeId,formData).subscribe(
+      (res:{
+  message: string
+  status: string
+  url: string
+  detail: {
+  message: string
+  name: string
+  status: number
+}
+})=>{
+  this.notification.showNotification('check', '#52c41a', res.message);
+  this.modalVisible = false
+  window.location.reload()
+},
+error =>{
+  this.notification.showNotification('warning',
+          '#eb2f96', error.error.message);
+}
+    )
+  }
+
+  beforeUpload = (file: NzUploadFile): Observable<boolean> =>
+    new Observable((observer: Observer<boolean>) => {
+      const maxSize =
+        file.type === 'application/pdf' ? 50 * 1024 * 1024 : 2 * 1024 * 1024;
+      const isLt2M = file.size! < maxSize;
+      console.log(file.size);
+      if (!isLt2M) {
+        this.notification.showNotification(
+          'warning',
+          '#eb2f96',
+          'File too large'
+        );
+        observer.complete();
+        return;
+      }
+      this.pictureList.push(file);
+      observer.next(isLt2M);
+      observer.complete();
+    });
 }
--- src/app/shared/service/employee/employee.service.ts
+++ src/app/shared/service/employee/employee.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -29,5 +30,17 @@ export class EmployeeService {
     return this.http.post<any>(this.baseUrl + '/employee', payload, {
       headers: this.getCustomHeaders(),
     });
+  }
+
+  deleteEmployee(employeeId:string):Observable<any>{
+    return this.http.delete(this.baseUrl+'/employee/'+employeeId,{headers:this.getCustomHeaders()})
+  }
+
+  uploadImage(employeeId:string,payload:FormData):Observable<any>{
+    const header = new HttpHeaders({
+      // 'Content-Type': 'multipart/form-data',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    });
+    return this.http.post<any>(this.baseUrl +'/employee/upload/'+employeeId,payload,{headers:header})
   }
 }
